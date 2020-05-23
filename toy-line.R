fddd9e5eff13150d0f3bfda9c11758f754ade400
putueddy <bli.ipei@gmail.com>
2020-05-23 09:27:41 +0700

    Initial

---
+++ toy-line.R
@@ -0,0 +1,11 @@
+n <- 40
+a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(n)
+y <- a + b * x + rnorm(n, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+write(avg_x, "avg_x.txt")
+plot(x,y)
+abline(a, b, col = "purple")
+dev.print(pdf, "toy_line_plot.pdf")
